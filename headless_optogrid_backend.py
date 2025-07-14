329401ae5788b49d9f0fec4c236a73abf70aa2e6
Daniel Mac <danielmac@eduroam-int-dhcp-97-100-185.ucl.ac.uk>
2025-07-14 14:30:28 +0100

    save

--- headless_optogrid_backend.py
+++ headless_optogrid_backend.py
@@ -168,7 +168,7 @@ class HeadlessOptoGridClient:
         # ZMQ setup
         self.zmq_context = zmq.Context()
         self.zmq_socket = self.zmq_context.socket(zmq.REP)
-        ip = self.get_ip()
+        ip = get_ip()
         self.zmq_socket.bind(f"tcp://{ip}:5555")
         self.logger.info(f"ZMQ server listening on tcp://{ip}:5555...")
         
@@ -205,17 +205,7 @@ class HeadlessOptoGridClient:
         self.running = True
         
 
-    def get_ip():
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            # doesn't have to be reachable
-            s.connect(('10.255.255.255', 1))
-            IP = s.getsockname()[0]
-        except Exception:
-            IP = '127.0.0.1'
-        finally:
-            s.close()
-        return IP
+
 
     def setup_logging(self):
         """Setup logging configuration"""
@@ -837,6 +827,18 @@ class HeadlessOptoGridClient:
             self.logger.info("ZMQ closed")
         except Exception as e:
             self.logger.error(f"ZMQ cleanup error: {e}")
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 async def main():
     """Main entry point"""
